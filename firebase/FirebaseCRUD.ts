62be12c74d0b2d0097a128b7ccb69892ed288387
Raul Zarza <raulzarza.dev@gmail.com>
2022-10-04 17:11:52 -0600

    add linters

---
+++ firebase/FirebaseCRUD.ts
@@ -0,0 +1,392 @@
+import {
+  ref,
+  uploadBytesResumable,
+  getDownloadURL
+} from 'firebase/storage'
+import { getAuth } from 'firebase/auth'
+import { format as fnsFormat } from 'date-fns'
+import { v4 as uidGenerator } from 'uuid'
+
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  onSnapshot,
+  Query,
+  query,
+  setDoc,
+  Timestamp,
+  updateDoc,
+  where
+} from 'firebase/firestore'
+import { db, storage } from '.'
+import { Dates } from 'firebase-dates-util'
+import { es } from 'date-fns/locale'
+
+type Target =
+  | 'firebase'
+  | 'milliseconds'
+  | 'date'
+  | 'fieldDate'
+export class FirebaseCRUD {
+  constructor(private collectionName: string = '') { }
+
+  static format = (
+    date: string | number | Date,
+    stringFormat = 'dd/MM/yy'
+  ): string => {
+    if (!date) {
+      console.error('not a date')
+      return 'NaD'
+    }
+    const objectDate = new Date(date)
+    function isValidDate (
+      d: string | number | Date
+    ): boolean {
+      return d instanceof Date && !isNaN(d as any)
+    }
+
+    if (isValidDate(objectDate)) {
+      return fnsFormat(
+        new Date(
+          objectDate.setMinutes(
+            objectDate.getMinutes() +
+            objectDate.getTimezoneOffset()
+          )
+        ),
+        stringFormat,
+        { locale: es }
+      )
+    } else {
+      console.error('date is not valid date')
+      return 'NaD'
+    }
+  }
+
+  static validateFiters (filters: any[], collectionName: string) {
+    if (!filters)
+      return console.error('Should have filters implentade')
+    if (!Array.isArray(filters))
+      return console.error('filter is not an array', {
+        collectionName
+      })
+
+    //* Validate insede each filter and find if any a the values is invalid
+    filters.map(filter => {
+      //* Looks like firebase define a function unsolved if the value of 
+      if (typeof filter._a === 'function') {
+        return console.error('unvalid data', { segment: filter.fa.segments[0], collectionName })
+      }
+    })
+
+    return filters
+  }
+
+
+  static dateToFirebase (date: string): Timestamp | null {
+    const dateFormated =
+      FirebaseCRUD.transformAnyToDate(date)
+    if (!dateFormated) return null
+    return Timestamp.fromDate(dateFormated)
+  }
+
+  static deepFormatFirebaseDates (
+    object: any,
+    target: 'timestamp' | 'number' | 'date' | 'fieldDate'
+  ) {
+    return Dates.deepFormatObjectDates(object, target)
+  }
+
+  static uploadFile = (
+    file: Blob | Uint8Array | ArrayBuffer,
+    fieldName = '',
+    cb = (
+      progress: number = 0,
+      downloadURL: string | null = null
+    ): void => { }
+  ) => {
+    const storageRef = (path = '') => ref(storage, path)
+    const uuid = uidGenerator()
+    const imageRef = storageRef(`${fieldName}/${uuid}`)
+    const uploadTask = uploadBytesResumable(imageRef, file)
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred /
+            snapshot.totalBytes) *
+          100
+        console.log('Upload is ' + progress + '% done')
+        cb(progress, null)
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused')
+            break
+          case 'running':
+            console.log('Upload is running')
+            break
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then(
+          (downloadURL) => {
+            console.log('File available at', downloadURL)
+            cb(100, downloadURL)
+          }
+        )
+      }
+    )
+    /*   uploadBytes(storageRef(storagePath), file).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+      } */
+  }
+
+  static normalizeDocs = (docs = []) =>
+    docs?.map((doc) => this.normalizeDoc(doc))
+
+  static normalizeDoc = (doc: any) => {
+    if (!doc?.exists()) return null // The document  not exist
+    const data = doc.data()
+    const id = doc.id
+
+    const res = FirebaseCRUD.deepFormatFirebaseDates(
+      data,
+      'number'
+    )
+
+    return {
+      id,
+      ...res
+    }
+  }
+
+  static formatResponse = (
+    ok: boolean,
+    type: string,
+    res: any
+  ) => {
+    if (!ok) throw new Error(type)
+    const formatedType = type.toUpperCase()
+    return { type: formatedType, ok, res }
+  }
+
+  async setDoc (itemId: string, newItem: object) {
+    const currentUser = getAuth().currentUser
+
+    const item = {
+      id: itemId,
+      updatedAt: new Date(),
+      createdAt: new Date(),
+      userId: currentUser?.uid,
+      ...newItem
+    }
+    await setDoc(doc(db, this.collectionName, itemId), item)
+    return { ...item }
+  }
+
+  async create (item: object) {
+    const currentUser = getAuth().currentUser
+
+    const newItem = {
+      updatedAt: new Date(),
+      createdAt: new Date(),
+      userId: currentUser?.uid,
+      ...item
+    }
+
+    const itemDatesToFirebaseTimestamp =
+      FirebaseCRUD.deepFormatFirebaseDates(
+        newItem,
+        'number'
+      )
+    console.log(itemDatesToFirebaseTimestamp)
+
+    return await addDoc(
+      collection(db, this.collectionName),
+      itemDatesToFirebaseTimestamp
+    )
+      .then((res) =>
+        FirebaseCRUD.formatResponse(
+          true,
+          `${this.collectionName}_CREATED`,
+          res
+        )
+      )
+      .catch((err) => console.error(err))
+  }
+
+  async update (itemId: string, item: object) {
+    const newItem = {
+      ...FirebaseCRUD.deepFormatFirebaseDates(
+        { ...item, updatedAt: new Date() },
+        'number'
+      )
+    }
+    // console.log(newItem)
+    return await updateDoc(
+      doc(db, this.collectionName, itemId),
+      newItem
+    )
+      .then((res) =>
+        FirebaseCRUD.formatResponse(
+          true,
+          `${this.collectionName}_UPDATED`,
+          res
+        )
+      )
+      .catch((err) => console.error(err))
+  }
+
+  async delete (itemId: string) {
+    return await deleteDoc(
+      doc(db, this.collectionName, itemId)
+    )
+      .then((res) =>
+        FirebaseCRUD.formatResponse(
+          true,
+          `${this.collectionName}_DELETED`,
+          res
+        )
+      )
+      .catch((err) => console.error(err))
+  }
+
+  async get (itemId: string) {
+    /**
+     * get a single document from the collection
+     * @param itemId the id of the document to get
+     */
+    const ref = doc(db, this.collectionName, itemId)
+    const docSnap = await getDoc(ref)
+    return FirebaseCRUD.normalizeDoc(docSnap)
+  }
+  async getMany (filters: any[]) {
+
+    /**
+     * * get all documents in a collection implmementing filters
+     * @param filters: where(itemField,'==','value')
+     */
+    FirebaseCRUD.validateFiters(filters, this.collectionName)
+    const q: Query = query(
+      collection(db, this.collectionName),
+      ...filters
+    )
+
+    const querySnapshot = await getDocs(q)
+    const res: ({ id: any } | null)[] = []
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      //console.log(doc.id, " => ", doc.data());
+      res.push(FirebaseCRUD.normalizeDoc(doc))
+    })
+    return res
+  }
+
+  async listen (itemId: string, cb: CallableFunction) {
+    if (!itemId) return console.error('invalid value', { itemId })
+    const q = doc(db, this.collectionName, itemId)
+    onSnapshot(q, (doc) => {
+      cb(FirebaseCRUD.normalizeDoc(doc))
+    })
+  }
+
+  async listenDocs (filters: any, cb: CallableFunction) {
+    /**
+     * listen all documents in a collection implmementing filters
+     * @param filters: where(itemField,'==','value')
+     */
+    FirebaseCRUD.validateFiters(filters, this.collectionName)
+    const q = query(
+      collection(db, this.collectionName),
+      ...filters
+    )
+
+    onSnapshot(q, (querySnapshot) => {
+      const res: any[] = []
+      querySnapshot.forEach((doc) => {
+        res.push(FirebaseCRUD.normalizeDoc(doc))
+      })
+      cb(res)
+    })
+  }
+
+  async listenCurrentUserDocs (cb: CallableFunction) {
+    const userId = getAuth().currentUser?.uid
+    this.listenDocs([where('userId', '==', userId)], cb)
+  }
+
+  async listenDocsByFilters (
+    filters: any,
+    cb: CallableFunction
+  ) {
+    /**
+     * listen all documents in a collection implmementing filters
+     * @param filters[]: where(itemField,'==','value')
+     */
+
+    FirebaseCRUD.validateFiters(filters, this.collectionName)
+
+    const q = query(
+      collection(db, this.collectionName),
+      ...filters
+    )
+
+    onSnapshot(q, (querySnapshot) => {
+      const res: any[] = []
+      querySnapshot.forEach((doc) => {
+        res.push(FirebaseCRUD.normalizeDoc(doc))
+      })
+      cb(res)
+    })
+  }
+
+  async listenAll (cb: CallableFunction) {
+    /**
+     * listen all documents in a collection
+     *
+     */
+    const q = query(collection(db, this.collectionName))
+
+    onSnapshot(q, (querySnapshot) => {
+      const res: any[] = []
+      querySnapshot.forEach((doc) => {
+        res.push(FirebaseCRUD.normalizeDoc(doc))
+      })
+      cb(res)
+    })
+  }
+
+  static transformAnyToDate = (
+    date: unknown
+  ): Date | null => {
+    if (!date) return null
+    if (date instanceof Timestamp) {
+      return date.toDate()
+    } else if (date instanceof Date) {
+      return date
+    } else if (typeof date === 'number') {
+      return new Date(date)
+    } else if (typeof date === 'string') {
+      let aux = new Date(date)
+      if (isNaN(aux.getTime())) {
+        return null
+      } else {
+        return aux
+      }
+    } else {
+      console.error('date is not valid date')
+      return null
+    }
+  }
+}
---
+++ firebase/user.js
@@ -0,0 +1,149 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup
+} from 'firebase/auth'
+import { auth } from '.'
+import { getUser, listenUser, setUser } from './Users'
+
+export async function signUp({ email, password }) {
+  return await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  )
+    .then(({ user }) => {
+      return { email, password }
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
+
+export async function signIn({ email, password }) {
+  return await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  ).then((userCredential) => userCredential)
+}
+
+export async function googleLogin() {
+  const provider = new GoogleAuthProvider()
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential =
+        GoogleAuthProvider.credentialFromResult(result)
+      const token = credential.accessToken
+      // The signed-in user info.
+      const user = result.user
+
+      // console.log(user)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code
+      const errorMessage = error.message
+      // The email of the user's account used.
+      const email = error.customData.email
+      // The AuthCredential type that was used.
+      const credential =
+        GoogleAuthProvider.credentialFromError(error)
+      // ...
+    })
+}
+
+const createUser = (user) => {
+  console.log(user)
+  const {
+    emailVerified,
+    photoURL,
+    phoneNumber,
+    providerData,
+    uid,
+    email,
+    displayName
+  } = user
+
+  const newUser = {
+    id: uid,
+    name: displayName,
+    displayName,
+    email: email,
+    emailVerified,
+    image: photoURL,
+    photoURL: photoURL,
+    phone: phoneNumber,
+    phoneNumber,
+    providerData
+  }
+  setUser(newUser.id, { ...newUser })
+    .then((res) => {
+      console.log('user created')
+    })
+    .catch((err) => {
+      console.error('create user error')
+    })
+
+  return newUser
+}
+
+export function authStateChanged(...props) {
+  const cb = props?.pop()
+  return onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      listenUser(user.uid, (dbUser) => {
+        if (dbUser) {
+          cb({ uid: user.uid, ...dbUser })
+        } else {
+          createUser(user)
+        }
+      })
+    } else {
+      cb(null)
+      console.log('not logged')
+    }
+  })
+}
+
+export async function sendRecoverPasswordEmail({
+  email,
+  domain
+}) {
+  const DOMAIN = 'pueblonomada.page.link'
+  const actionCodeSettings = {
+    url: `https://${DOMAIN}/?email=${email}`,
+    iOS: {
+      bundleId: DOMAIN
+    },
+    android: {
+      packageName: DOMAIN,
+      installApp: true,
+      minimumVersion: '12'
+    },
+    handleCodeInApp: true
+  }
+  return await sendPasswordResetEmail(
+    auth,
+    email,
+    actionCodeSettings
+  )
+    .then((res) => {
+      console.log('Email sent', { res })
+      return { res }
+      // Password reset email sent!
+      // ..
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
+export function logout() {
+  return signOut(auth)
+}

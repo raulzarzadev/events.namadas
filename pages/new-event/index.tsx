3ea0d7e9065b6f6ee88634d7f7035c3c86393696
Raul Zarza <raulzarza.dev@gmail.com>
2022-11-09 17:52:05 -0700

    todo list

--- pages/new-event/index.tsx
+++ pages/new-event/index.tsx
@@ -1,13 +1,15 @@
-import FormEvent from "@comps/events/formEvent_V3";
-import PrivatePage from "HOCS/PrivatePage";
+import FormEvent from '@comps/events/formEvent_V3';
+import PrivatePage from 'HOCS/PrivatePage';
 
 const NewEvent = () => {
-  
+  // TODO create just a form where you can chose what kind of event, out side or inside, and mor details to publish it
+  // TODO create an interface to import events from other pages,
+  // TODO search events and publish it
   return (
-      <PrivatePage>
-        <FormEvent />
-      </PrivatePage>
+    <PrivatePage>
+      <FormEvent />
+    </PrivatePage>
   );
-}
+};
 
 export default NewEvent;

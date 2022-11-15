f4ac17f04c1480b85c833438a42144aeed251618
Raul Zarza <raulzarza.dev@gmail.com>
2022-11-15 00:22:52 -0700

    fix bredcrums, hidde optional params

--- components/Breadcrumb/index.tsx
+++ components/Breadcrumb/index.tsx
@@ -6,22 +6,25 @@ import { useRouter } from 'next/router';
 const Breadcrumb = () => {
   const router = useRouter();
 
-  const getCrumbs = (path:string) => {
-    if(path==='/') return [{route:'/', label:'home'}]
-    const pathItems = path.split('/');
-    return pathItems.reduce((prev:any, curr:any, i:number, arr:any[]) => {
+  const getCrumbs = (path: string) => {
+    if (path === '/') return [{ route: '/', label: 'home' }];
+    const pathWithoutOptionalParams = path.split('?')[0];
+    const pathItems = pathWithoutOptionalParams.split('/');
+    return pathItems.reduce((prev: any, curr: any, i: number, arr: any[]) => {
       //console.log({prev, curr})
       if (curr === '') return [...prev, { route: '/', label: 'home' }];
       const accumulatedRoute = arr.slice(0, i + 1).join('/');
       return [...prev, { route: accumulatedRoute, label: curr }];
     }, []);
   };
-  const crumbs = getCrumbs(router.asPath)
+  const crumbs = getCrumbs(router.asPath);
   return (
     <div>
       <div className="text-sm breadcrumbs">
         <ul>
-         {crumbs.map(crumb=><Crumb href={crumb.route} label={crumb.label} key={crumb.route}/>)}
+          {crumbs.map((crumb) => (
+            <Crumb href={crumb.route} label={crumb.label} key={crumb.route} />
+          ))}
         </ul>
       </div>
     </div>

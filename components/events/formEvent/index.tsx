743618ec54526dc877456f1265bea4e5ebc01669
Raul Zarza <raulzarza.dev@gmail.com>
2022-11-28 04:47:38 -0700

    modify formEvent

--- components/events/formEvent/index.tsx
+++ components/events/formEvent/index.tsx
@@ -1,5 +1,5 @@
-// import InputFiles, { SetImagesOps } from '@comps/inputs/inputFiles_V2'
-import { Event } from '@firebase/Events/event.model'
+import InputFiles, { SetImagesOps } from '@comps/inputs/inputFiles_V2'
+import { Event as EventType } from '@firebase/Events/event.model'
 import { createEvent, updateEvent } from '@firebase/Events/main'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
@@ -7,77 +7,77 @@ import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import myFormatDate from 'utils/myFormatDate'
 
-// import SubEventsSection from './SubEventSection_v2'
-// import EventDates from './EventDates'
-// import StepperForm from './StepperForm'
-// import BasicInformation from './BasicInformation'
-// import ParticipantsSection from './ParticipantsSection'
-// import AddLinksSection from './AddLinksSection'
+import SubEventsSection from './SubEventSection_v2'
+import EventDates from './EventDates'
+import StepperForm from './StepperForm'
+import BasicInformation from './BasicInformation'
+import ParticipantsSection from './ParticipantsSection'
+import AddLinksSection from './AddLinksSection'
 
-const FormEvent = ({ event }: { event?: Partial<Event> }) => {
-  // const eventAlreadyExist = event?.id
-  // const router = useRouter()
-  // const currentDate = new Date().getTime()
-  // const defaultValues: Partial<Event> = {
-  //   date: myFormatDate(currentDate, 'datetime'),
-  //   finishAt: myFormatDate(currentDate, 'datetime'),
-  //   status: 'HIDDEN',
-  //   subscriptionsOptions: {
-  //     finishAt: myFormatDate(currentDate, 'inputDate'),
-  //     startAt: myFormatDate(currentDate, 'inputDate'),
-  //     limit: 0,
-  //     acceptSubscriptions: false,
-  //     acceptTerms: false
-  //   }
-  // }
+const FormEvent = ({ event }: { event?: Partial<EventType> }) => {
+  const eventAlreadyExist = event?.id
+  const router = useRouter()
+  const currentDate = new Date().getTime()
+  const defaultValues: Partial<EventType> = {
+    date: myFormatDate(currentDate, 'datetime'),
+    finishAt: myFormatDate(currentDate, 'datetime'),
+    status: 'HIDDEN',
+    subscriptionsOptions: {
+      finishAt: myFormatDate(currentDate, 'inputDate'),
+      startAt: myFormatDate(currentDate, 'inputDate'),
+      limit: 0,
+      acceptSubscriptions: false,
+      acceptTerms: false
+    }
+  }
 
-  // const methods = useForm({
-  //   defaultValues: event ? { ...defaultValues, ...event } : defaultValues
-  // })
+  const methods = useForm({
+    defaultValues: event ? { ...defaultValues, ...event } : defaultValues
+  })
 
-  // const { handleSubmit, watch } = methods
+  const { handleSubmit, watch, setValue } = methods
 
-  // const hardSubmit = () => {
-  //   console.log('hard submit')
-  //   setFormStatus(FORM_LABELS.loading)
-  //   handleSubmit((props: any) => {
-  //     console.log('submit')
-  //     onSubmit(props)
-  //   })()
-  // }
+  const hardSubmit = () => {
+    console.log('hard submit')
+    setFormStatus(FORM_LABELS.loading)
+    handleSubmit((props: any) => {
+      console.log('submit')
+      onSubmit(props)
+    })()
+  }
 
-  // const formValues = watch()
-  // const onSubmit = (data: any): void => {
-  //   // console.log(data);
-  //   setFormStatus(FORM_LABELS.loading)
-  //   event?.id // eventAlreadyExist
-  //     ? updateEvent(event?.id, data)
-  //         .then((res) => {
-  //           console.log(res)
-  //         })
-  //         .catch((err) => {
-  //           setFormStatus(FORM_LABELS.error)
-  //           console.error(err)
-  //         })
-  //         .finally(() => {
-  //           setFormStatus(FORM_LABELS.saved)
-  //         })
-  //     : createEvent(data)
-  //         .then((res) => {
-  //           const id = res?.res?.id
-  //           if (typeof id === 'string') {
-  //             router.push(`/events/${id}/edit`)
-  //           }
-  //           console.log(res)
-  //         })
-  //         .catch((err) => {
-  //           setFormStatus(FORM_LABELS.error)
-  //           console.error(err)
-  //         })
-  //         .finally(() => {
-  //           setFormStatus(FORM_LABELS.saved)
-  //         })
-  // }
+  const formValues = watch()
+  const onSubmit = (data: any): void => {
+    // console.log(data);
+    setFormStatus(FORM_LABELS.loading)
+    event?.id // eventAlreadyExist
+      ? updateEvent(event?.id, data)
+          .then((res) => {
+            console.log(res)
+          })
+          .catch((err) => {
+            setFormStatus(FORM_LABELS.error)
+            console.error(err)
+          })
+          .finally(() => {
+            setFormStatus(FORM_LABELS.saved)
+          })
+      : createEvent(data)
+          .then((res) => {
+            const id = res?.res?.id
+            if (typeof id === 'string') {
+              router.push(`/events/${id}/edit`)
+            }
+            console.log(res)
+          })
+          .catch((err) => {
+            setFormStatus(FORM_LABELS.error)
+            console.error(err)
+          })
+          .finally(() => {
+            setFormStatus(FORM_LABELS.saved)
+          })
+  }
 
   const FORM_LABELS = {
     error: {
@@ -118,71 +118,101 @@ const FormEvent = ({ event }: { event?: Partial<Event> }) => {
     }
   }
 
-  // useEffect(() => {
-  //   if (eventAlreadyExist) {
-  //     setFormStatus(FORM_LABELS?.edit)
-  //   } else {
-  //     setFormStatus(FORM_LABELS.save)
-  //   }
-  // }, [])
+  useEffect(() => {
+    if (eventAlreadyExist) {
+      setFormStatus(FORM_LABELS?.edit)
+    } else {
+      setFormStatus(FORM_LABELS.save)
+    }
+  }, [])
 
-  // const [formStatus, setFormStatus] = useState(FORM_LABELS.clean)
+  const [formStatus, setFormStatus] = useState(FORM_LABELS.clean)
 
-  // const handleSetImages = (images: any[], setImagesOps?: SetImagesOps) => {
-  //   if (setImagesOps?.uploading) setFormStatus(FORM_LABELS.loading)
-  //   if (images.length) {
-  //     setValue('images', images)
-  //     hardSubmit()
-  //   }
-  // }
-  // console.log({ errors });
+  const handleSetImages = (images: any[], setImagesOps?: SetImagesOps) => {
+    if (setImagesOps?.uploading) setFormStatus(FORM_LABELS.loading)
+    if (images.length) {
+      setValue('images', images)
+      hardSubmit()
+    }
+  }
 
-  // console.log(formValues);
+  const STEPS = [
+    {
+      label: 'Information',
+      Component: <BasicInformation />
+    },
+    {
+      label: 'Dates',
+      Component: <EventDates />
+    },
+    {
+      label: 'Sub Events',
+      Component: <SubEventsSection />
+    },
+    {
+      label: 'Participants',
+      helperText: 'Esta seccion esta deshabilitada por ahora',
+      Component: <ParticipantsSection />
+    },
+    {
+      label: 'Related ',
+      helperText: `Add some links to help users find more information about others
+        events, sponsors and social media`,
+      Component: (
+        <>
+          <AddLinksSection />
+        </>
+      )
+    },
+    {
+      label: 'Media',
+      Component: (
+        <InputFiles
+          fieldName="eventImage"
+          label="Add some images "
+          images={formValues?.images}
+          setImages={handleSetImages}
+          displayAs="row"
+          // disabled={disabled}
+        />
+      )
+    }
+  ]
 
-  // const STEPS = [
-  //   {
-  //     label: 'Information',
-  //     Component: <BasicInformation />
-  //   },
-  //   {
-  //     label: 'Dates',
-  //     Component: <EventDates />
-  //   },
-  //   {
-  //     label: 'Sub Events',
-  //     Component: <SubEventsSection />
-  //   },
-  //   {
-  //     label: 'Participants',
-  //     helperText: 'Esta seccion esta deshabilitada por ahora',
-  //     Component: <ParticipantsSection />
-  //   },
-  //   {
-  //     label: 'Related ',
-  //     helperText: `Add some links to help users find more information about others
-  //       events, sponsors and social media`,
-  //     Component: (
-  //       <>
-  //         <AddLinksSection />
-  //       </>
-  //     )
-  //   },
-  //   {
-  //     label: 'Media',
-  //     Component: (
-  //       <InputFiles
-  //         fieldName="eventImage"
-  //         label="Add some images "
-  //         images={formValues?.images}
-  //         setImages={handleSetImages}
-  //         displayAs="row"
-  //         // disabled={disabled}
-  //       />
-  //     )
-  //   }
-  // ]
+  console.log(formValues)
 
-  return <div className="relative"></div>
+  return (
+    <div className="relative">
+      <Head>
+        <title>{formStatus.title}</title>
+      </Head>
+      <h2 className="text-xl font-bold text-center mt-4 whitespace-pre-wrap">
+        {formValues?.title}
+      </h2>
+      <p className="text-center mb-4">
+        {formStatus?.title || 'Create new event'}
+      </p>
+
+      <form
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onSubmit={handleSubmit(onSubmit)}
+        data-test-id="event-form"
+        data-test-op={eventAlreadyExist ? 'editing-event' : 'new-event'}
+        className={'mb-24 max-w-lg mx-auto px-1'}
+      >
+        <StepperForm steps={STEPS} />
+        <div className="flex justify-around fixed w-full bottom-0 bg-base-200 p-2 border-t-4 border-t-base-100 left-0 right-0">
+          <button
+            className="btn btn-primary"
+            data-test-id="submit-event-form"
+            disabled={formStatus.disabled}
+          >
+            {formStatus.button}
+          </button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default FormEvent
